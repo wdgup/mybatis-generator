5f7c3d6146e6a88486be604556644cff1035c2e8
wangdaogang <1229706777@qq.com>
2019-10-12 00:43:35 +0800

    Initial commit

---
+++ src/main/java/com/wdg/MybatisGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.wdg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisGeneratorApplication.class, args);
+    }
+
+}
